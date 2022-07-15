ee7d1a472f06113e8ad577128db11b0ef3e90fbb
Ihor Bobyr <igorvladimirovichbobir@gmail.com>
2022-07-16 00:37:28 +0300

    fix incorrect filename generation

--- webpack.config.js
+++ webpack.config.js
@@ -39,14 +39,14 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'img/[name].[ext]',
+          filename: 'img/[name][ext]',
         },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'fonts/[name].[ext]',
+          filename: 'fonts/[name][ext]',
         },
       },
     ],
