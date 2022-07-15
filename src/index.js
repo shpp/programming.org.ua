24f78ba8af83c1b742d258c009a448935dfc2f44
Ihor Bobyr <igorvladimirovichbobir@gmail.com>
2022-07-16 02:08:28 +0300

    implement partners section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './index.scss';
+import Swiper, { Autoplay } from 'swiper';
+import 'swiper/css';
 
 ('use strict');
 
@@ -8,3 +10,25 @@ globalThis.programmingOrgUa = {
   openMobileMenu: () => document.querySelector('header > .right')?.classList.add('mobile-menu-opened'),
   closeMobileMenu: () => document.querySelector('header > .right')?.classList.remove('mobile-menu-opened'),
 };
+
+new Swiper(document.querySelector('.partners-slider'), {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  breakpoints: [
+    [576, 1],
+    [768, 2],
+    [992, 3],
+    [1199, 4],
+    [1200, 5],
+  ].reduce(
+    (breakPointsConfig, [resolution, numberOfSlides]) => ({
+      ...breakPointsConfig,
+      [resolution]: { slidesPerView: numberOfSlides },
+    }),
+    {}
+  ),
+  modules: [Autoplay],
+});
