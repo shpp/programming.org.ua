3bc9c0299732cf10b6df52b8255e6ad86129f569
Ihor Bobyr <igorvladimirovichbobir@gmail.com>
2022-08-12 01:36:53 +0300

    improve grouping by context

--- src/index.js
+++ src/index.js
@@ -106,85 +106,86 @@ createApp({
   },
 }).mount('.feedbacks');
 
-// todo: review and simplify
-class Accordion {
-  constructor(el) {
-    this.el = el;
-    this.summary = el.querySelector('summary');
-    this.content = el.querySelector('.content');
+document.querySelectorAll('details').forEach(
+  (el) =>
+    new (class {
+      // todo: review and simplify
+      constructor(el) {
+        this.el = el;
+        this.summary = el.querySelector('summary');
+        this.content = el.querySelector('.content');
 
-    this.animation = null;
-    this.isClosing = false;
-    this.isExpanding = false;
-    this.summary.addEventListener('click', (e) => this.onClick(e));
-  }
-
-  onClick(e) {
-    e.preventDefault();
-    this.el.style.overflow = 'hidden';
-    if (this.isClosing || !this.el.open) {
-      this.open();
-    } else if (this.isExpanding || this.el.open) {
-      this.shrink();
-    }
-  }
-
-  shrink() {
-    this.isClosing = true;
-    const startHeight = `${this.el.offsetHeight}px`;
-    const endHeight = `${this.summary.offsetHeight}px`;
-
-    this.animation && this.animation.cancel();
-
-    this.animation = this.el.animate(
-      {
-        height: [startHeight, endHeight],
-      },
-      {
-        duration: 400,
-        easing: 'ease-out',
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+        this.summary.addEventListener('click', (e) => this.onClick(e));
       }
-    );
 
-    this.animation.onfinish = () => this.onAnimationFinish(false);
-    this.animation.oncancel = () => (this.isClosing = false);
-  }
-
-  open() {
-    this.el.style.height = `${this.el.offsetHeight}px`;
-    this.el.open = true;
-    window.requestAnimationFrame(() => this.expand());
-  }
-
-  expand() {
-    this.isExpanding = true;
-    const startHeight = `${this.el.offsetHeight}px`;
-    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
-
-    this.animation && this.animation.cancel();
-
-    this.animation = this.el.animate(
-      { height: [startHeight, endHeight] },
-      {
-        duration: 400,
-        easing: 'ease-out',
+      onClick(e) {
+        e.preventDefault();
+        this.el.style.overflow = 'hidden';
+        if (this.isClosing || !this.el.open) {
+          this.open();
+        } else if (this.isExpanding || this.el.open) {
+          this.shrink();
+        }
       }
-    );
-    this.animation.onfinish = () => this.onAnimationFinish(true);
-    this.animation.oncancel = () => (this.isExpanding = false);
-  }
 
-  onAnimationFinish(open) {
-    this.el.open = open;
-    this.animation = null;
-    this.isClosing = false;
-    this.isExpanding = false;
-    this.el.style.height = '';
-    this.el.style.overflow = '';
-  }
-}
+      shrink() {
+        this.isClosing = true;
+        const startHeight = `${this.el.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight}px`;
 
-document.querySelectorAll('details').forEach((el) => new Accordion(el));
+        this.animation && this.animation.cancel();
+
+        this.animation = this.el.animate(
+          {
+            height: [startHeight, endHeight],
+          },
+          {
+            duration: 400,
+            easing: 'ease-out',
+          }
+        );
+
+        this.animation.onfinish = () => this.onAnimationFinish(false);
+        this.animation.oncancel = () => (this.isClosing = false);
+      }
+
+      open() {
+        this.el.style.height = `${this.el.offsetHeight}px`;
+        this.el.open = true;
+        window.requestAnimationFrame(() => this.expand());
+      }
+
+      expand() {
+        this.isExpanding = true;
+        const startHeight = `${this.el.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+
+        this.animation && this.animation.cancel();
+
+        this.animation = this.el.animate(
+          { height: [startHeight, endHeight] },
+          {
+            duration: 400,
+            easing: 'ease-out',
+          }
+        );
+        this.animation.onfinish = () => this.onAnimationFinish(true);
+        this.animation.oncancel = () => (this.isExpanding = false);
+      }
+
+      onAnimationFinish(open) {
+        this.el.open = open;
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+        this.el.style.height = '';
+        this.el.style.overflow = '';
+      }
+    })(el)
+);
 
 // todo: get translations url from config
 fetch('https://data.kowo.space/data/programming.org.ua/translations/en.json')
