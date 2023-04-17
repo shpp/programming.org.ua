5a07974dd362d0bcfab2d3ea9c90fccd4f6a9e2f
Ihor Bobyr <igorvladimirovichbobir@gmail.com>
2023-04-17 13:12:51 +0300

    update formatting

--- webpack.config.js
+++ webpack.config.js
@@ -251,9 +251,9 @@ module.exports = async (_, { mode = 'development' }) => ({
       )
     )),
     new webpack.DefinePlugin({
-      __VUE_OPTIONS_API__ : true,
-      __VUE_PROD_DEVTOOLS__: false
-    })
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
   ],
   module: {
     rules: [
