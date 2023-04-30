221be2f037490ffe39b89c2b25ec2238cc242e11
kateboyko <kboyko1999@gmail.com>
2023-04-30 09:05:54 +0300

    implemented feedback form page

---
+++ src/image-upload.js
@@ -0,0 +1,78 @@
+export default {
+    props: ['name', 'placeholder', 'required'],
+    data: () => ({
+        preview: '',
+        filename: '',
+        dragActive: false
+    }),
+    mounted() {
+        this.initDragNDrop();
+    },
+    methods: {
+        renderPreview(file) {
+            const reader = new FileReader();
+
+            reader.addEventListener('load', () => {
+                this.filename = file.name;
+                this.preview = reader.result;
+            });
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        },
+        clearInput() {
+            this.filename = '';
+            this.preview = '';
+            this.$refs.input.value = null;
+        },
+        initDragNDrop() {
+            const dropzone = this.$refs.dropzone;
+
+            // Prevent default drag behaviors
+            ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+            dropzone.addEventListener(eventName, (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+            }, false);
+            document.body.addEventListener(eventName, (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+            }, false);
+            });
+
+            // Highlight drop zone when item is dragged over it
+            ['dragenter', 'dragover'].forEach(eventName => {
+                dropzone.addEventListener(eventName, () => this.dragActive = true, false);
+            });
+
+            ['dragleave', 'drop'].forEach(eventName => {
+                dropzone.addEventListener(eventName, () => this.dragActive = false, false);
+            });
+
+            // Handle dropped files
+            dropzone.addEventListener('drop', (e) => this.renderPreview(e.dataTransfer.files[0]), false);
+        }
+    },
+    template: ` <div class="form-element-image" ref="dropzone">
+                    <input
+                        type="file"
+                        :id="name"
+                        :name="name"
+                        :required="required"
+                        accept="image/*"
+                        ref="input"
+                        @change="e => renderPreview(e.target.files[0])">
+                    <label
+                        :for="name"
+                        v-if="!preview"
+                        :class="{dragActive}"
+                        v-html="placeholder"
+                    ></label>
+                    <figure v-else>
+                        <img :src="preview"/>
+                        <figcaption>{{filename}}</figcaption>
+                        <button @click="clearInput"><i class="fa fa-xmark"></i></button>
+                    </figure>
+                </div>`
+}
--- src/pages/feedback-form-page/index.js
+++ src/pages/feedback-form-page/index.js
@@ -1,1 +1,53 @@
 import './index.scss';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import ImageUpload from '../../image-upload';
+
+('use strict');
+
+createApp({
+    components: {
+        ImageUpload
+    },
+    data: () => ({
+        validated: false,
+        role: 'student',
+        loading: false,
+        responseCode: null
+    }),
+    methods: {
+        validateAndSubmit() {
+            this.validated = true;
+
+            setTimeout(() => {
+                const isFormValid = this.$refs.form.reportValidity();
+
+                if (!isFormValid) {
+                    return;
+                }
+
+                const formData = new FormData(this.$refs.form);
+                this.loading = true;
+                fetch(' https://back.programming.org.ua/api/feedback/create', {
+                    method: 'POST',
+                    body: formData,
+                })
+                    .then(res => res.json())
+                    .then(({status}) => {
+                        this.responseCode = status;
+                        this.loading = false;
+
+                        if (status === 200) {
+                            // not sure if it's needed
+                            // this.$refs.form.reset();
+                            this.validated = false;
+                        }
+                    })
+                    .catch(err => {
+                        this.responseCode = err.code;
+                        this.loading = false;
+                    })
+            }, 0)
+
+        },
+    }
+}).mount('main');
