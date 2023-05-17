1f981e7e6f68f123bb8e99ac02db54ed1aa4df34
kateboyko <kboyko1999@gmail.com>
2023-05-17 22:05:08 +0300

    fixed opening the details after clicking a link on the support page

--- src/pages/support-us-page/index.js
+++ src/pages/support-us-page/index.js
@@ -2,3 +2,10 @@ import './index.scss';
 
 location.hash === '#detailed' &&
   document.querySelector('.support-description').classList.add('detailed');
+
+window.addEventListener(
+  'hashchange',
+  () =>
+    location.hash === '#detailed' &&
+    document.querySelector('.support-description').classList.add('detailed')
+);
