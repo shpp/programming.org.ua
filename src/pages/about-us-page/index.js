203ed25204eda6b863a53a64f8c267a81f82eca7
kateboyko <kboyko1999@gmail.com>
2023-05-26 22:42:55 +0100

    added history slider

--- src/pages/about-us-page/index.js
+++ src/pages/about-us-page/index.js
@@ -1,6 +1,6 @@
 import './index.scss';
 import '../../template-partials/question-form/question-form';
-import Swiper, { Autoplay, Navigation } from 'swiper';
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
 import 'swiper/css';
 
 function setupTeamSlider() {
@@ -10,9 +10,9 @@ function setupTeamSlider() {
     autoplay: {
       disableOnInteraction: false,
     },
+    slidesPerView: 2,
     spaceBetween: 16,
     breakpoints: {
-      350: { slidesPerView: 2 },
       576: { slidesPerView: 3 },
       992: { slidesPerView: 5 },
       1200: { slidesPerView: 7 },
@@ -25,8 +25,31 @@ function setupTeamSlider() {
   });
 }
 
+function setupHistorySlider() {
+  console.log('call swiper');
+  new Swiper(document.querySelector('.history-slider'), {
+    loop: false,
+    spaceBetween: 32,
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 4 },
+      1400: { slidesPerView: 5 },
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.history-slider-next',
+      prevEl: '.history-slider-previous',
+    },
+    modules: [Pagination, Navigation],
+  });
+}
+
 function main() {
   setupTeamSlider();
+  setupHistorySlider();
 }
 
 main();
