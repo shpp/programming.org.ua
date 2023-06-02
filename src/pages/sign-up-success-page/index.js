ace0a16fb8fb42bf148265a63e84df841dc781db
Ihor Bobyr <igorvladimirovichbobir@gmail.com>
2023-06-03 00:15:23 +0300

    add anketa last page

--- src/pages/sign-up-success-page/index.js
+++ src/pages/sign-up-success-page/index.js
@@ -1,1 +1,13 @@
 import './index.scss';
+
+const pageLocalStorageKey = 'anketa/last';
+
+document.querySelector('.content .email').innerHTML = `(${localStorage.getItem(
+  `${pageLocalStorageKey}.student.email`
+)})`;
+
+const pageVisitedKey = `${pageLocalStorageKey}.isVisited`;
+
+!localStorage.getItem(pageVisitedKey)
+  ? localStorage.setItem(pageVisitedKey, 'true')
+  : (location.href = '/');
