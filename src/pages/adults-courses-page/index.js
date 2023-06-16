7f1298946fd1f28cc83957e028c100e07ba819b2
kateboyko <kboyko1999@gmail.com>
2023-06-16 19:00:38 +0100

    added FAQ blocks for courses pages

--- src/pages/adults-courses-page/index.js
+++ src/pages/adults-courses-page/index.js
@@ -1,1 +1,8 @@
 import './index.scss';
+import Accordion from '../../template-partials/accordion';
+
+function main() {
+  new Accordion('.faq ul', { openItems: 'all' });
+}
+
+main();
--- src/pages/courses-page/index.js
+++ src/pages/courses-page/index.js
@@ -1,2 +1,9 @@
 import './index.scss';
 import '../../template-partials/question-form/question-form';
+import Accordion from '../../template-partials/accordion';
+
+function main() {
+  new Accordion('.faq ul', { openItems: 'all' });
+}
+
+main();
